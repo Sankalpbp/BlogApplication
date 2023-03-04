08c68662a7addf1147809f33643da0c52d54d38b
Sankalpbp <sankalpnitj@gmail.com>
2023-03-04 22:40:11 +0530

    Initial configuration of the blog application, setting up the database and other properties.

---
+++ src/main/java/io/sankalp/blogapplication/BlogApplication.java
@@ -0,0 +1,13 @@
+package io.sankalp.blogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
