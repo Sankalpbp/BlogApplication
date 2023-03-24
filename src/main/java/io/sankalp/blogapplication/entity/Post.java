f78dcfe67b00a80ee44899f65f71ae80133d07ed
Sankalpbp <sankalpnitj@gmail.com>
2023-03-25 01:13:12 +0530

    Added Comment resource to the project and set its Many-to-One relationship with the Post resource.

--- src/main/java/io/sankalp/blogapplication/entity/Post.java
+++ src/main/java/io/sankalp/blogapplication/entity/Post.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -43,5 +46,8 @@ public class Post {
             nullable = false
     )
     private String content;
+
+    @OneToMany ( mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true )
+    private Set<Comment> comments = new HashSet<>();
 
 }
