c3fe7d8d8f090894998cb54e829a3c3530f085c2
Sankalpbp <sankalpnitj@gmail.com>
2023-03-27 10:50:04 +0530

    Adding comments of post to the response of the Post.

--- src/main/java/io/sankalp/blogapplication/payload/PostDTO.java
+++ src/main/java/io/sankalp/blogapplication/payload/PostDTO.java
@@ -2,10 +2,14 @@ package io.sankalp.blogapplication.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDTO {
     private Long id;
     private String title;
     private String description;
     private String content;
+
+    private Set<CommentDTO> comments;
 }
