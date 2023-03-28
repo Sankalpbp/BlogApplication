221da7040125b9f68b4ba5a4eae086ad97376f50
Sankalpbp <sankalpnitj@gmail.com>
2023-03-28 12:39:01 +0530

    Handling AccessDeniedException

--- src/main/java/io/sankalp/blogapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/io/sankalp/blogapplication/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -47,6 +48,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                                                        request.getDescription ( false ) );
 
         return new ResponseEntity<> ( errorDetails, HttpStatus.INTERNAL_SERVER_ERROR );
+    }
+
+    @ExceptionHandler ( AccessDeniedException.class )
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException ( AccessDeniedException exception,
+                                                                      WebRequest request ) {
+        ErrorDetails errorDetails = new ErrorDetails ( new Date (),
+                                                       exception.getMessage (),
+                                                       request.getDescription ( false ) );
+
+        return new ResponseEntity<> ( errorDetails, HttpStatus.UNAUTHORIZED );
     }
 
     @Override
