f9b98206d2001d99434bd05f7f5aca2291480b2b
Sankalpbp <sankalpnitj@gmail.com>
2023-03-29 01:06:30 +0530

    Fixing some typos

--- src/main/java/io/sankalp/blogapplication/security/JwtTokenProvider.java
+++ src/main/java/io/sankalp/blogapplication/security/JwtTokenProvider.java
@@ -15,10 +15,10 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    @Value ( "${ app.jwt-secret }" )
+    @Value ( "${app.jwt-secret}" )
     private String jwtSecret;
 
-    @Value ( "${ app-jwt-expiration-milliseconds }")
+    @Value ( "${app-jwt-expiration-milliseconds}")
     private long jwtExpirationDate;
 
     public String generateToken ( Authentication authentication ) {
