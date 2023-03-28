23345bc5c9402865709f35db1adb187efee5948a
Sankalpbp <sankalpnitj@gmail.com>
2023-03-28 12:19:05 +0530

    Created in-memory users and gave access to all the APIs for testing.

--- src/main/java/io/sankalp/blogapplication/config/SecurityConfig.java
+++ src/main/java/io/sankalp/blogapplication/config/SecurityConfig.java
@@ -2,12 +2,24 @@ package io.sankalp.blogapplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder ( ) {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity security ) throws Exception {
@@ -17,6 +29,23 @@ public class SecurityConfig {
                 .httpBasic ( Customizer.withDefaults () );
 
         return security.build ();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService ( ) {
+        UserDetails main = User.builder ()
+                               .username ( "sankalp" )
+                               .password ( passwordEncoder ( ).encode ( "sankalp" ) )
+                               .roles ( "USER" )
+                               .build ();
+
+        UserDetails admin = User.builder ()
+                                .username ( "admin" )
+                                .password ( passwordEncoder ( ).encode ( "admin" ) )
+                                .roles ( "ADMIN" )
+                                .build ();
+
+        return new InMemoryUserDetailsManager ( main, admin );
     }
 
 }
