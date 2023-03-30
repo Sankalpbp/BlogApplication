b72048cc3a0ad4d12753a239673d8702794d4a0e
Sankalpbp <sankalpnitj@gmail.com>
2023-03-30 11:03:12 +0530

    Adding profiles and inserting roles into the Role table.

--- src/main/java/io/sankalp/blogapplication/BlogApplication.java
+++ src/main/java/io/sankalp/blogapplication/BlogApplication.java
@@ -1,11 +1,15 @@
 package io.sankalp.blogapplication;
 
+import io.sankalp.blogapplication.entity.Role;
+import io.sankalp.blogapplication.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +35,7 @@ import org.springframework.context.annotation.Bean;
         url = "https://github.com/Sankalpbp/BlogApplication"
     )
 )
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
@@ -40,5 +44,20 @@ public class BlogApplication {
     @Bean
     public ModelMapper getModelMapper () {
         return new ModelMapper ();
+    }
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run ( String... args ) throws Exception {
+        Role adminRole = new Role ();
+        adminRole.setName ( "ROLE_ADMIN" );
+        roleRepository.save ( adminRole );
+
+        Role userRole = new Role ();
+        userRole.setName ( "USER_ROLE" );
+        roleRepository.save ( userRole );
+
     }
 }
